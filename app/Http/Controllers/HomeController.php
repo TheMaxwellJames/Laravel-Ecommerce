4af78413b9a811ad22531bef20eebb6e4031e437
Maxwell James <maxichanj@gmail.com>
2023-06-20 09:38:33 +0100

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -220,52 +220,6 @@ public function index()
 
 
 
-                    //  $cart=new cart;
-
-                    //  $cart->name=$user->name;
-
-                    //  $cart->email=$user->email;
-
-                    //  $cart->phone=$user->phone;
-
-                    //  $cart->address=$user->address;
-
-                    //  $cart->user_id=$user->id;
-
-
-                    //  $cart->product_title=$product->title;
-
-
-
-
-                    //  if($product->discount_price!==null)
-                    //  {
-                    //     $cart->price=$product->discount_price * $request->quantity;
-                    //  }
-
-                    //  else
-                    //  {
-                    //     $cart->price=$product->price * $request->quantity;
-                    //  }
-                     
-
-
-                    //  $cart->image=$product->image;
-
-                    //  $cart->product_id=$product->id;
-
-
-
-
-                    //  $cart->quantity=$request->quantity;
-
-
-
-
-                    //  $cart->save();
-
-                    //  return redirect()->back();
-
                      
 
 
